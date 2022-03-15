27a27c9696b99c36eb0177b5340bcd894ffbd5c7
Alex Miroshkin <mirogindev@gmail.com>
2022-03-15 17:53:10 +0300

    Change int64 custom scalar type

--- gqbuilder/scalars.go
+++ gqbuilder/scalars.go
@@ -10,21 +10,30 @@ var Int64Scalar = graphql.NewScalar(graphql.ScalarConfig{
 	Name:        "Int64",
 	Description: ``,
 	Serialize: func(value interface{}) interface{} {
-		t, ok := value.(int64)
-		if !ok {
+		switch value := value.(type) {
+		case *int64:
+			return *value
+		case int64:
+			return value
+		default:
 			panic("Value is not int64")
 		}
-		return t
+
+		return nil
 	},
 	// parseValue: gets invoked to parse client input that was passed through variables.
 	// value is plain type
 	ParseValue: func(value interface{}) interface{} {
-		v, ok := value.(int64)
-		if !ok {
+		switch value := value.(type) {
+		case *int64:
+			return *value
+		case int64:
+			return value
+		default:
 			panic("Value is not int64")
 		}
 
-		return v
+		return nil
 	},
 	// parseLiteral: gets invoked to parse client input that was passed inline in the query.
 	// value is ast.Value
